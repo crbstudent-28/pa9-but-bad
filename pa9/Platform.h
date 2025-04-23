0321a7b51e1d353bd5e0d266636ebefc3565a818
radia <radia@C-DESKTOP>
2025-04-23 01:57:59 -0700

    Add project files.

---
+++ pa9/Platform.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+class Platform {
+public:
+	sf::RectangleShape* rect;
+	Platform() {
+		rect = new sf::RectangleShape;
+	}
+	Platform(float x, float y) {
+		rect = new sf::RectangleShape;
+		rect->setSize(sf::Vector2f(60, 10));
+		rect->setPosition(sf::Vector2f(x, y));
+		rect->setFillColor(sf::Color::Green);
+	}
+	virtual int trigger(sf::RectangleShape* doodler, sf::Vector2f* dv) {
+		dv->y -= 30;
+		return 0;
+	}
+	sf::RectangleShape getrect() {
+		return *rect;
+	}
+	~Platform() {
+		delete rect;
+		rect = nullptr;
+	}
+};
+
+class SuperPlatform : public Platform {
+public:
+	SuperPlatform(float x, float y) {
+		rect = new sf::RectangleShape;
+		rect->setSize(sf::Vector2f(60, 10));
+		rect->setPosition(sf::Vector2f(x, y));
+		rect->setFillColor(sf::Color::Yellow);
+	}
+	
+	int trigger(sf::RectangleShape* doodler, sf::Vector2f* dv) override {
+		dv->y -= 60;
+		return 0;
+	}
+};
+
+class BreakPlatform : public Platform {
+public:
+	BreakPlatform(float x, float y) {
+		rect = new sf::RectangleShape;
+		rect->setSize(sf::Vector2f(60, 10));
+		rect->setPosition(sf::Vector2f(x, y));
+		rect->setFillColor(sf::Color::Magenta);
+	}
+
+	int trigger(sf::RectangleShape* doodler, sf::Vector2f* dv) override {
+		dv->y -= 30;
+		return 1;
+	}
+};
